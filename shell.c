d03e09b1d4f4972f5398d125cdd712658038ef34
Siddhi Chitgopkar <ssc67916@uga.edu>
2022-07-28 11:16:40 -0400

    new versions

--- shell.c
+++ shell.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define BUFFSIZE 100
 
-int LaunchProcess(char* string[]){
+int LaunchProcess(char* string[]) {
     int ex;
     pid_t pid;
+
     if ((pid = fork()) < 0) { // error
         perror("fork");
     } else if (pid == 0) { // in child process
+
         ex = execvp(string[0], &string[0]);
         if (ex == -1) {
             perror(string[0]);
@@ -23,18 +26,25 @@ int LaunchProcess(char* string[]){
 } //LaunchProcess
 
 
-int ChangeDir(char* string[]) {
-    if (string[1] == NULL) {
+int ChangeDir(char* string[], int count) {
+    if (count == 1) {
         chdir("..");
-        chdir("..");
-    } //if
-    const char* elemone = string[1];
-    if (strcmp(elemone, ".") == 0) {
-        return 0;
-    } else if (strcmp(elemone, "..") == 0) {
         chdir("..");
     } else {
-        chdir(elemone);
+        const char* elemone = string[1];
+        if (strcmp(elemone, ".") == 0) {
+            return 0;
+        } else if (strcmp(elemone, "..") == 0) {
+            chdir("..");
+        } else if (strcmp(elemone, "~") == 0) {
+            chdir(getenv("HOME"));
+        } else {
+            if (chdir(elemone) == -1) {
+                perror(string[1]);
+            } else {
+                chdir(elemone);
+            } //else
+        } //else
     } //else
     return 1;
 } //ChangeDir
@@ -42,25 +52,20 @@ int ChangeDir(char* string[]) {
 int main(int argc, char* argv[]) {
     const char* homedir = getenv("HOME");
     chdir(homedir);
-
-    char* string[120];
     int n;
-    int count = 0;
+    int c = 0;
     int flag = 0;
     char path[BUFFSIZE];
 
-
-
-    while (homedir[count] != '\0') {
-        count = count + 1;
+    while (homedir[c] != '\0') {
+        c = c + 1;
     } //while
 
-
-
     while (flag != 1) {
+        char* string[120];
+        int count = 0;
         getcwd(path, BUFFSIZE);
         printf("1730sh:");
-        //int count = 0;
         int numspaces = 0;
         char* buf = malloc(BUFFSIZE);
         setbuf(stdout, NULL);
@@ -72,16 +77,14 @@ int main(int argc, char* argv[]) {
             char* ret = strstr(path, (getenv("HOME")));
             if (ret) {
                 printf("~");
-                printf("%d\n", count);
-                while (path[count] != '\0') {
-                    printf("%c", path[count]);
-                    count++;
+                int tempc = c;
+                while (path[tempc] != '\0') {
+                    printf("%c", path[tempc]);
+                    tempc++;
                 } //while
             } else {
                 printf("%s", path);
             } //else
-// char *token = strtok(path, getenv("HOME"));
-            //printf(token);
         } //else
 
         printf("$ ");
@@ -105,29 +108,67 @@ int main(int argc, char* argv[]) {
             count++;
             token = strtok(NULL, delim);
         } //while
-        (string[numspaces + 1]) = NULL;
+        (string[count]) = NULL;
 
-        if (strcmp(string[0], "exit") == 0 ) {
-            free(buf);
-            exit(0);
+        int in = 0;
+        int out = 0;
+        int doublein = 0;
+        int doubleout = 0;
+
+        int inindex = 0;
+        int outindex = 0;
+        int doubleinindex = 0;
+        int doubleoutindex = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (strcmp(string[i],"<") == 0) {
+                in = 1;
+                inindex = i;
+            } //if
+            if (strcmp(string[i],">") == 0) {
+                out = 1;
+                outindex = i;
+            } //if
+            if (strcmp(string[i],"<<") == 0) {
+                doublein = 1;
+                doubleinindex = i;
+            } //if
+            if (strcmp(string[i],">>") == 0) {
+                doubleout = 1;
+                doubleoutindex = i;
+            } //if
+        } //for
+
+        int redirectflag = 0;
+        if (doubleout == 1 || doublein == 1 || in == 1 || out == 1) {
+            redirectflag = 1;
         } //if
 
-        if (strcmp(string[0], "cd") == 0) {
-            ChangeDir(string);
+        if (count == 0);
+        else if (strcmp(string[0], "exit") == 0 ) {
+            free(buf);
+            exit(0);
+        } else if (strcmp(string[0], "cd") == 0) {
+            ChangeDir(string, count);
+        } else if (redirectflag == 1) {
+            /*
+            if (in == 1) {
+                //printf("%d\n", inindex);
+                //printf("input");
+                int fdi = open(string[inindex], O_RDONLY);
+                dup2(fdi, STDIN_FILENO);
+            } //if
+            */
+/*
+            if (out == 1) {
+                printf("output");
+                int fdo = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                dup2(fdo, STDOUT_FILENO);
+            } //if
+            */
         } else {
             LaunchProcess(string);
         } //else
-/*
-        //for loop to go through all elements
-        //if statement to see if one of then has a <
-        int fdinput = open("input.txt", O_RDONLY);
-        dup2(fdi, STDIN_FILENO);
-
-        // could also use O_APPEND instead of O_TRUNC, as needed
-        int fdoutput = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        dup2(fdo, STDOUT_FILENO);
-*/
-
         free(buf);
     } //while
     return EXIT_SUCCESS;
